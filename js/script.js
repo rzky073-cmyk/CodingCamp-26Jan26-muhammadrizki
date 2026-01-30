5333f95cff5dd48fd2a4138df6b68577b5997cb0
rzky073-cmyk <rzky.073@gmail.com>
2026-01-31 01:11:54 +0700

    Creating the "message me" data structures & their oprations

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ welcomeMessage ();
 function welcomeMessage() {
     const userRespons = prompt("Welcome to Rizki Company! what is your name ?");
 
+    const el = document.getElementById("welcome-speech");
     if (userRespons === null || userRespons.trim() === "") {
-        alert("Hello, Guest! Feel free to explore our website.");
-    }else
+        el.innerText = "Hello, Guest! Feel Free to Explore My Website.";
+    }else{
 
-    document.getElementById("welcome-speech").innerText = `Hello, ${userRespons}! Welcome to Rizki Company.` ;
+    el.innerText = `Hello, ${userRespons}! Welcome to My Website.` ;
+    }
 }
