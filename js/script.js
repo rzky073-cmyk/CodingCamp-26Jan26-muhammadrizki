e692753fb9d9bede54d501b56d8d6285068b9a7d
rzky073-cmyk <rzky.073@gmail.com>
2026-01-31 09:46:02 +0700

    update html sturctures, improve css styling, add sticky navbar and smooth scrolling, add social media link to footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 welcomeMessage ();
 
 function welcomeMessage() {
-    const userRespons = prompt("Welcome to Rizki Company! what is your name ?");
+    const userRespons = prompt("Welcome to My Website! what is your name ?");
 
     const el = document.getElementById("welcome-speech");
     if (userRespons === null || userRespons.trim() === "") {
